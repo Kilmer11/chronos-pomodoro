0a86465c79b53a117725b784cd14eeb6d1ef9ded
Kilmer <kilmerandrade45@gmail.com>
2025-10-03 11:40:03 -0300

    feat: implementation sort tasks

--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -1,18 +1,49 @@
 import styles from './styles.module.css';
 
+import { useState } from 'react';
 import { TrashIcon } from 'lucide-react';
 import { MainTemplate } from '../../app/templates/MainTemplate';
 import { Container } from '../../components/Container';
 import { Heading } from '../../components/Heading';
 import { DefaultButton } from '../../components/DefaultButton';
 import { useTaskContext } from '../../app/contexts/TaskContext/useTaskContext';
-import type { TaskModel } from '../../shared/models/taskModel';
 import { formatDate } from '../../shared/utils/formatDate';
 import { getTaskStatus } from '../../shared/utils/getTaskStatus';
 import { taskTypeDictionary } from '../../shared/utils/taskTypeDictionary';
+import { sortTasks, type SortTasksProps } from '../../shared/utils/sortTask';
+import type { TaskModel } from '../../shared/models/taskModel';
 
 export function History() {
   const { state } = useTaskContext();
+  const [sortTasksOptions, setSortTasksOptions] = useState<SortTasksProps>(
+    () => {
+      return {
+        tasks: sortTasks({ tasks: state.tasks }),
+        direction: 'asc',
+        field: 'name',
+      };
+    },
+  );
+
+  function handleSortTasksOptions({ field }: Pick<SortTasksProps, 'field'>) {
+    const newDirection = sortTasksOptions.direction === 'desc' ? 'asc' : 'desc';
+
+    setSortTasksOptions({
+      tasks: sortTasks({
+        tasks: sortTasksOptions.tasks,
+        direction: newDirection,
+        field,
+      }),
+      direction: newDirection,
+      field,
+    });
+  }
+
+  const cabecalhos = [
+    { label: 'Tarefa', field: 'name' },
+    { label: 'Duração', field: 'duration' },
+    { label: 'Data', field: 'startDate' },
+  ];
 
   return (
     <MainTemplate>
@@ -35,16 +66,25 @@ export function History() {
           <table>
             <thead>
               <tr>
-                <th>Task</th>
-                <th>Duration</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th>Type</th>
+                {cabecalhos.map(({ label, field }) => (
+                  <th
+                    onClick={() =>
+                      handleSortTasksOptions({
+                        field: field as keyof TaskModel,
+                      })
+                    }
+                    className={styles.thSort}
+                  >
+                    {label}
+                  </th>
+                ))}
+                <th className={styles.thSort}>Status</th>
+                <th className={styles.thSort}>Type</th>
               </tr>
             </thead>
 
             <tbody>
-              {state.tasks.map((task: TaskModel) => (
+              {sortTasksOptions.tasks.map((task: TaskModel) => (
                 <tr key={task.id}>
                   <td>{task.name}</td>
                   <td>{task.duration} min</td>
